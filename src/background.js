53739be23822b561bfaa67f6de92d0f9fd7ba8da
tahyo27 <tahyo27@gmail.com>
2024-03-20 19:35:39 +0900

    배경화면

---
+++ src/background.js
@@ -0,0 +1,17 @@
+const setRandomBackground = () => {
+    const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg",
+      "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg"];
+  
+    const chosenImage = images[Math.floor(Math.random() * images.length)];
+    const bgImage = `/img/${chosenImage}`;
+  
+    document.body.style.backgroundImage = `url(${bgImage})`;
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.height = "100vh";
+    document.body.style.width = "100vw";
+    ///document.body.style.overflow = "hidden";
+  };
+  
+  export default setRandomBackground;
