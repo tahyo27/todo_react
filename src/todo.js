49c5145ad48f4f39bb47620a06b6e2d76f250785
tahyo27 <tahyo27@gmail.com>
2024-04-11 21:54:13 +0900

    로그인 todo list 저장기능

--- src/todo.js
+++ src/todo.js
@@ -4,12 +4,23 @@ function Todo() {
     const [todos, setTodos] = useState([]);
     const [inputValue, setInputValue] = useState("");
     const storedUsername = localStorage.getItem("username"); 
+    
+    useEffect(() => {
+      if(storedUsername != null) {
+          console.log("로컬 저장 테스트"); 
+          const storedTodosKey = `${storedUsername}_todos`;
+          const storedTodos = JSON.parse(localStorage.getItem(storedTodosKey));
+          console.log("length" + storedTodos.length);
+          if(storedTodos.length !== 0) {
+            setTodos(storedTodos);
+          }
+          console.log(storedTodos);
+          // 저장된 todo가 있으면 JSON.parse를 사용해 배열로 변환
+      } else {
+          console.log("값 널임 테스트");
+      }
+  }, [storedUsername]); // storedUsername이 변경될 때만 이 코드를 실행
 
-    if(storedUsername == null) {
-        console.log("값 널임 테스트");
-    } else {
-        console.log("로컬 저장 테스트")
-    }
     //const todoList 
 
     //하고싶은것 이름과 todoList 연동해서 저장
@@ -17,8 +28,17 @@ function Todo() {
     const addTodo = () => {
         if(inputValue.trim() !== "") {
             setTodos(prevTodos => [...prevTodos, inputValue]);
+            setInputValue(''); // 입력값 비우기
         }
+        
     }
+     useEffect(() => {
+        if (storedUsername) {
+            // todos 상태를 문자열로 변환하여 저장
+            localStorage.setItem(`${storedUsername}_todos`, JSON.stringify(todos));
+        }
+    }, [todos, storedUsername]);
+
     return (
     <div>
         <input 
